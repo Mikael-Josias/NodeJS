5421ee52afbe3c1fbd83f220bcad04a51aae8989
Mikael Josias Rodrigues <mikaeljosias2002@gmail.com>
2020-11-03 16:39:15 -0300

    9.File Restructuring

--- server.js
+++ server.js
@@ -12,17 +12,7 @@ mongoose.connect(
 );
 requireDir("./src/models"); 
 
-const Product = mongoose.model('Product');
-
-//Primeira Rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with reactive',
-        url: 'https://github.com/facebook/react-native' 
-    });
-
-    return res.send('Hello World');
-});
+//Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001); 
