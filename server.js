4b573d3fd3ed9982e3a77d2d5678a7d5dda9b049
Mikael Josias Rodrigues <mikaeljosias2002@gmail.com>
2020-11-03 14:09:21 -0300

    8.Create Product Model

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//Iniciando o APP
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+//Iniciando o MongoDB
+mongoose.connect(
+    'mongodb://localhost:27017/NodeJS', 
+    {useNewUrlParser: true}
+);
+requireDir("./src/models"); 
 
+const Product = mongoose.model('Product');
+
+//Primeira Rota
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with reactive',
+        url: 'https://github.com/facebook/react-native' 
+    });
+
+    return res.send('Hello World');
 });
 
-app.listen(3001);
+app.listen(3001); 
