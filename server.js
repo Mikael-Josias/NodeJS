9a32c8ba9cf200ca74957ce84ad4bbbd74f16ce3
Mikael Josias Rodrigues <mikaeljosias2002@gmail.com>
2020-11-04 12:56:43 -0300

    14.Add Cors

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //Iniciando o APP
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 //Iniciando o MongoDB
 mongoose.connect(
     'mongodb://localhost:27017/NodeJS', 
